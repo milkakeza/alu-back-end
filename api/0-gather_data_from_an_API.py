1468a477bbca17ac87de7246fffb572379e9bac8
milkakeza <m.isingizwe1@alustudent.com>
2024-10-19 13:56:27 +0200

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Python script that returns TODO list progress for a given employee ID
+Python script that returns TODO list progress for a given employee ID.
 """
 
-import requests  # type: ignore
+import requests # type: ignore
 from sys import argv
 
 
 def get_employee_info(employee_id):
     """
-    Get employee information by employee ID
+    Get employee information by employee ID.
     """
     url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/'
     response = requests.get(url)
@@ -18,7 +18,7 @@ def get_employee_info(employee_id):
 
 def get_employee_todos(employee_id):
     """
-    Get the TODO list of the employee by employee ID
+    Get the TODO list of the employee by employee ID.
     """
     url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
     response = requests.get(url)
@@ -27,32 +27,30 @@ def get_employee_todos(employee_id):
 
 def main(employee_id):
     """
-    Main function to fetch and display the TODO list progress of the employee
+    Main function to fetch and display the TODO list progress of the employee.
     """
     employee = get_employee_info(employee_id)
     employee_name = employee.get("name")
-
-    if len(employee_name) != 18:
-        print(f"Employee Name: Incorrect\n\n({len(employee_name)} chars long)")
-    else:
-        print(f"Employee Name: OK\n\n({len(employee_name)} chars long)")
 
     emp_todos = get_employee_todos(employee_id)
     tasks = {todo.get("title"): todo.get("completed") for todo in emp_todos}
 
     total_tasks = len(tasks)
-    completed_tasks = [completed for completed in tasks.values() if completed]
+    completed_tasks = [title for title, completed in tasks.items() if completed]
     completed_tasks_count = len(completed_tasks)
 
     print(f"Employee {employee_name} is done with tasks"
           f"({completed_tasks_count}/{total_tasks}):")
-    for title, completed in tasks.items():
-        if completed:
-            print(f"\t {title}")
+    for title in completed_tasks:
+        print(f"\t {title}")
 
 
 if __name__ == "__main__":
     if len(argv) > 1:
-        main(argv[1])
+        try:
+            employee_id = int(argv[1])
+            main(employee_id)
+        except ValueError:
+            print("Employee ID must be an integer.")
     else:
         print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
